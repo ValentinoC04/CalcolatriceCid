4a241fb75b96eba71dca472e9c12d61fa6e74809
Valentino Conte <v.conte12@studenti.unisa.it>
2025-10-06 10:26:56 +0200

    Ho creato una calcolatrice che fa addizione e sottrazione

---
+++ src/main/java/org/example/Calcolatrice.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class Calcolatrice {
+    public Calcolatrice() {
+    }
+    protected int  Addizione(int a, int b) {
+        return a + b;
+    }
+    protected int  Sotttrazione(int a, int b) {
+        return a - b;
+    }
+}
