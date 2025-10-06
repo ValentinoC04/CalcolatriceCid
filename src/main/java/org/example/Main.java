b64023b604ee0cf172df82fdbfcfab69b2f870fc
Valentino Conte <v.conte12@studenti.unisa.it>
2025-10-06 10:26:47 +0200

    Ho creato una calcolatrice che fa addizione e sottrazione

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,26 @@
 package org.example;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Calcolatrice c=new Calcolatrice();
+        Scanner s=new Scanner(System.in);
+        System.out.println("Cosa vuoi fare ? Addizione(A) o Sottrazione(S)?");
+        String risposta=s.nextLine();
+        if(risposta.equals("A")){
+            System.out.println("Dammi il valore di del primo numero");
+            int a=s.nextInt();
+            System.out.println("Dammi il valore di del secondo  numero");
+            int b=s.nextInt();
+            System.out.println("Il risultato è: "+c.Addizione(a,b));
+        }
+        else if(risposta.equals("S")){
+            System.out.println("Dammi il valore di del primo numero");
+            int a=s.nextInt();
+            System.out.println("Dammi il valore di del secondo  numero");
+            int b=s.nextInt();
+            System.out.println("Il risultato è: "+c.Sotttrazione(a,b));
+        }
     }
 }
